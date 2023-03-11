aae5ebbbaf0624dad689a6fe2151588abfb2c896
Tran Thanh Vinh <tranthanhvinh242@gmail.com>
2023-03-11 19:09:00 +0700

    Lab3-Frontend-Products

---
+++ Modules/Lab3/Frontend/Products.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../Assets/Styles/Lab3-Products.css">
+    <title>Products</title>
+</head>
+<body>
+    <div id="container" class="container">
+        <div class="products col-12 ta_c m_a">
+            <h1 class="title">All Products</h1>
+            <div class="wrapper__products col-12 d_f jc_s">
+                <div class="product__box col-3 ta_c">
+                    <img src="../../../Assets/Images/Lab-3/cornflakes-4907490_1920.jpg" alt="">
+                    <h1>Product Name</h1>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                    <button class="col-5 c_p">View Details</button>
+                </div>
+
+                <div class="product__box col-3 ta_c">
+                    <img src="../../../Assets/Images/Lab-3/honey-1958464_1920.jpg" alt="">
+                    <h1>Product Name</h1>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                    <button class="col-5 c_p">View Details</button>
+                </div>
+
+                <div class="product__box col-3 ta_c">
+                    <img src="../../../Assets/Images/Lab-3/milk-1887234_1920.jpg" alt="">
+                    <h1>Product Name</h1>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                    <button class="col-5 c_p">View Details</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
